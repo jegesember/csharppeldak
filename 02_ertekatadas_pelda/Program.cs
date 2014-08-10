f9753370ee96424e56cfe18155087ba0e5aa4428
webmaster442 <ruzsinszki.gabor@gmail.com>
2014-08-10 11:16:47 +0200

    3. cikkhez tartozó forráskódok 1. része

---
+++ 02_ertekatadas_pelda/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _02_ertekatadas_pelda
+{
+    class osztaly
+    {
+        public double ertek;
+    }
+
+    class Program
+    {
+        static void ErtekatadoPelda(double ertek)
+        {
+            ertek = 2.1;
+        }
+
+        static void ReferenciaPelda(osztaly referencia)
+        {
+            referencia.ertek = 2.1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Érték típus példa");
+            Console.WriteLine();
+
+            double ertek = 3.14;
+            Console.WriteLine("Függvény hívás előtt az ertek: {0}", ertek);
+            ErtekatadoPelda(ertek);
+            Console.WriteLine("Függvény hívás utan az ertek: {0}", ertek);
+
+            Console.WriteLine();
+            Console.WriteLine("Referencia típus példa");
+            Console.WriteLine();
+
+            osztaly o = new osztaly();
+            o.ertek = 3.14;
+            Console.WriteLine("Függvény hívás előtt az ertek: {0}", o.ertek);
+            ReferenciaPelda(o);
+            Console.WriteLine("Függvény hívás utan az ertek: {0}", o.ertek);
+        }
+    }
+}
