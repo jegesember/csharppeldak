97e9858e3b5a1f9aaccd204a7329c1e0a496f11e
webmaster442 <ruzsinszki.gabor@gmail.com>
2014-08-13 19:38:18 +0200

    3. cikkhez tartozó forráskód

---
+++ 04_adatbekeres_convert/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04_adatbekeres_convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bekérés és konvertálás példa");
+
+            Console.Write("Írj be egy számot: ");
+            string sor = Console.ReadLine();
+
+            Console.Write("Írj be egy lebegőpontos számot: ");
+            string sor2 = Console.ReadLine();
+
+            Console.WriteLine("A bekért szám +1: {0}", Convert.ToInt32(sor) + 1);
+            Console.WriteLine("A bekért lebegőpontos szám osztva 2-vel: {0}", Convert.ToDouble(sor2) / 2);
+
+        }
+    }
+}
---
+++ 05_adatbekeres_convert_hibakezelt/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05_adatbekeres_convert_hibakezelt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bekérés és konvertálás példa");
+
+            Console.Write("Írj be egy számot: ");
+            string sor = Console.ReadLine();
+
+            Console.Write("Írj be egy lebegőpontos számot: ");
+            string sor2 = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine("A bekért szám +1: {0}", Convert.ToInt32(sor) + 1);
+                Console.WriteLine("A bekért lebegőpontos szám osztva 2-vel: {0}", Convert.ToDouble(sor2) / 2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba történt!! Nem megfelelő a bevitt adat.");
+                Console.WriteLine("A hiba leirasa: {0}", ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("töltöttkáposzta");
+            }
+        }
+    }
+}
