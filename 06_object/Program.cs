c48204edcff2639290aa0a29ecc1d827b731fc92
webmaster442 <ruzsinszki.gabor@gmail.com>
2014-10-24 14:42:20 +0200

    további példák

---
+++ 06_object/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _06_object
+{
+    class Ojjektum
+    {
+        public int Ertek;
+
+        public Ojjektum(int value)
+        {
+            Ertek = value;
+        }
+
+        public override string ToString()
+        {
+            return "Én egy ojjektum vagyok. Értékem: " + Ertek.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Ojjektum) return obj.GetHashCode() == this.GetHashCode(); //return (obj as Ojjektum).Ertek == this.Ertek;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Ertek.GetHashCode();
+        }
+
+        public virtual void izé() { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ojjektum a = new Ojjektum(4);
+            Ojjektum b = new Ojjektum(5);
+            Ojjektum c = new Ojjektum(4);
+
+            Console.WriteLine("Ojjektum a: {0}", a);
+            Console.WriteLine("Ojjektum b: {0}", b);
+            Console.WriteLine("Ojjektum c: {0}", c);
+
+            Console.WriteLine("Ojjektum a és b egyenlősége: {0}", a.Equals(b));
+            Console.WriteLine("Ojjektum a és c egyenlősége: {0}", a.Equals(c));
+            Console.WriteLine("Ojjektum b és c egyenlősége: {0}", b.Equals(c));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 07_statikus_objektum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07_statikus_objektum
+{
+    static class Statikus
+    {
+        private static int Ertek; 
+
+        static Statikus()
+        {
+            Ertek = 44;
+        }
+
+        public static void Fuggveny()
+        {
+            Console.WriteLine("Statikus függvény, statikus osztályban. A tárolt érték: {0}", Ertek);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Statikus.Fuggveny();
+            Console.ReadKey();
+        }
+    }
+}
