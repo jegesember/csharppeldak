706d8ffce43cc398ac2867e5d7b12c176d8cf264
webmaster442 <ruzsinszki.gabor@gmail.com>
2014-10-29 15:25:53 +0100

    4. rész példakódjai

---
+++ 08_absztrakt_felulet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _08_absztrakt_felulet
+{
+    public abstract class AbsztraktOsztaly
+    {
+        public abstract void Bemutatkozas();
+    }
+
+    public interface IFelulet
+    {
+        string FeluletNeve();
+    }
+
+    public class Implementacio: AbsztraktOsztaly, IFelulet
+    {
+
+        public override void Bemutatkozas()
+        {
+            Console.WriteLine("Ez egy absztrakt osztályból örököltetett Implementáció");
+        }
+
+        public string FeluletNeve()
+        {
+            return "Az implentáció megvalósítja az IFelület felületet";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Implementacio teszt = new Implementacio();
+            teszt.Bemutatkozas();
+            Console.WriteLine(teszt.FeluletNeve());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 09_felulet_explicit/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _09_felulet_explicit
+{
+    interface Elso
+    {
+        void Bemutatkozik();
+    }
+
+    interface Masodik
+    {
+        void Bemutatkozik();
+    }
+
+    class Test: Elso, Masodik
+    {
+        public void Bemutatkozik()
+        {
+            Console.WriteLine("Én az első felület metódusa vagyok");
+            Console.WriteLine("Engem implicit módon deklaráltak");
+            Console.WriteLine("Ezért én vagyok az alapértelmezett");
+            Console.WriteLine("Ha a Bemutatkozik() metódust hívják");
+            Console.WriteLine();
+        }
+
+        void Masodik.Bemutatkozik()
+        {
+            Console.WriteLine("Én a második felület metódusa vagyok");
+            Console.WriteLine("Ezért engem csak explicit módon lehetett implementálni");
+            Console.WriteLine("Illetve engem csak típus konverzió után lehet meghívni");
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test teszt = new Test();
+            teszt.Bemutatkozik();
+            (teszt as Masodik).Bemutatkozik();
+            Console.ReadKey();
+        }
+    }
+}
