04a63c06a7826e582c280b21c74602c2cf76c05e
webmaster442 <ruzsinszki.gabor@gmail.com>
2014-11-02 11:23:33 +0100

    További példák

---
+++ 10_tulajdonsag/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _10_tulajdonsag
+{
+    public class Tulajdonsag
+    {
+        private int _ertek;
+
+        public int Ertek
+        {
+            get
+            {
+                Console.WriteLine("Az erteket lekerdeztek");
+                return _ertek;
+            }
+            set
+            {
+                _ertek = value;
+                Console.WriteLine("Az erteket beallitottak");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tulajdonsag teszt = new Tulajdonsag();
+            teszt.Ertek = 55;
+            Console.WriteLine(teszt.Ertek);
+        }
+    }
+}
---
+++ 11_anonim_tulajdonsag/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _11_anonim_tulajdonsag
+{
+    class NemAnonim
+    {
+        private int _ertek;
+
+        public NemAnonim()
+        {
+            _ertek = 33;
+        }
+
+        public int Tulajdonsag
+        {
+            get
+            {
+                return _ertek;
+            }
+        }
+    }
+
+    public class Anonim
+    {
+        public int Tulajdonsag
+        {
+            get;
+            private set;
+        }
+
+        public Anonim()
+        {
+            Tulajdonsag = 33;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NemAnonim n = new NemAnonim();
+            Anonim a = new Anonim();
+            Console.WriteLine(n.Tulajdonsag);
+            Console.WriteLine(a.Tulajdonsag);
+        }
+    }
+}
