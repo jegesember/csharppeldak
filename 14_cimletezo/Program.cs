3af23eba5434f1df91baad39d26ef334be37d445
webmaster442 <ruzsinszki.gabor@gmail.com>
2014-11-25 16:34:13 +0100

    6. rész forráskódja

---
+++ 14_cimletezo/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14_cimletezo
+{
+
+    /// <summary>
+    /// Kiegészítő függvények
+    /// </summary>
+    static class Kiegeszitok
+    {
+        /// <summary>
+        /// Szótár objektumhoz fűz hozzá egy elemet. Ha az elem létezik a megadott kulcsal, akkor értéket cserél
+        /// </summary>
+        /// <typeparam name="TKey">A kulcs típusa</typeparam>
+        /// <typeparam name="TValue">Az érték típusa</typeparam>
+        /// <param name="dictionary">A szótár</param>
+        /// <param name="key">A kulcs</param>
+        /// <param name="value">Az érték</param>
+        public static void AddEditItem<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (dictionary.ContainsKey(key)) dictionary[key] = value;
+            else dictionary.Add(key, value);
+        }
+    }
+
+    class Program
+    {
+        /// <summary>
+        /// Kerekítés szabályainak megfelelően kerekíti a bemeneti összeget
+        /// </summary>
+        /// <param name="bemenet">Bemeneti összeg</param>
+        /// <returns>Kerekített kimenet</returns>
+        private static long kerekit(long bemenet)
+        {
+            long kimenet = bemenet;
+            long maradek = bemenet % 10;
+            if (maradek >= 8) kimenet += (10 - maradek);
+            else if (maradek >= 6 && maradek <= 7) kimenet -= (maradek - 5);
+            else if (maradek >= 3 && maradek <= 4) kimenet += (5 - maradek);
+            else if (maradek >= 1 && maradek <= 2) kimenet -= maradek;
+            return kimenet;
+        }
+
+        /// <summary>
+        /// A megadott összeget címletekre bontja fel
+        /// </summary>
+        /// <param name="osszeg">a felbontandó összeg</param>
+        private static void Cimletez(long osszeg)
+        {
+            long hatravan = osszeg;
+            int[] cimletek = { 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5 };
+            Dictionary<int, long> cimletezett = new Dictionary<int, long>();
+            foreach (var cimlet in cimletek)
+            {
+                if (hatravan < cimlet) continue;
+                long mennyi = hatravan / cimlet;
+                hatravan -= mennyi * cimlet;
+                cimletezett.AddEditItem(cimlet, mennyi);
+                if (hatravan == 0) break;
+            }
+
+            Console.WriteLine("Az összeg címletezezve:");
+            foreach (var cimlet in cimletezett)
+            {
+                Console.WriteLine("{0} db {1} Ft-os", cimlet.Value, cimlet.Key);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("--------------------------------------------------");
+                Console.WriteLine("Címletező program");
+                Console.WriteLine("--------------------------------------------------");
+                Console.Write("Add meg a címletezendő összeget: ");
+                long input = Convert.ToInt64(Console.ReadLine());
+                input = kerekit(input);
+                Console.WriteLine("A cimletezeshez a kerekitett osszeg: {0}\n", input);
+                Cimletez(input);
+                Console.WriteLine("Nyomj egy gombot a kilepeshez...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba Történt!");
+                Console.WriteLine("A hiba leírása: {0}", ex.Message);
+            }
+        }
+    }
+}
--- 14_cimletezo/Properties/AssemblyInfo.cs
+++ 14_cimletezo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("14_letoltoprogram")]
+[assembly: AssemblyTitle("14_cimletezo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("14_letoltoprogram")]
+[assembly: AssemblyProduct("14_cimletezo")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d1d2f6e4-7638-44a9-8c54-fd7625b51a85")]
+[assembly: Guid("f478bb5d-afb4-4444-ba56-11eb4e5f32d1")]
 
 // Version information for an assembly consists of the following four values:
 //
